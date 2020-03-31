ef50536a49d8c331f24cd1295757d00b613ce7ce
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-03-31 21:37:32 +0300

    updated sudokuapp

---
+++ SudokuApp/src/main/java/sudokuapp/Main.java
@@ -0,0 +1,10 @@
+package sudokuapp;
+
+import javafx.application.Application;
+import sudokuapp.ui.SudokuUi;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(SudokuUi.class);
+    }
+}
---
+++ SudokuApp/src/main/java/sudokuapp/ui/SudokuUi.java
@@ -0,0 +1,13 @@
+package sudokuapp.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SudokuUi extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Sudoku");
+        stage.show();
+    }
+}
