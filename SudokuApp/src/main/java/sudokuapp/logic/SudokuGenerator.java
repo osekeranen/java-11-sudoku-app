13342f904e9c495e2d7457767c9179730c736a58
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-04-07 16:50:38 +0300

    finished sudoku generator

--- SudokuApp/src/main/java/sudokuapp/logic/SudokuGenerator.java
+++ SudokuApp/src/main/java/sudokuapp/logic/SudokuGenerator.java
@@ -10,18 +10,62 @@ public class SudokuGenerator {
     public int[][] generateSudoku() {
         int[][] sudoku = new int[9][9];
         
-        sudoku[0] = this.generateRandomisedLine();
+        sudoku[0] = this.generateRandomisedRow();
         
         for (int i = 1; i < 9; i++) {
             if (i == 3 || i == 6) {
-                sudoku[i] = this.shiftLine(sudoku[i - 1], 1);
-            } else sudoku[i] = this.shiftLine(sudoku[i - 1], 3);
+                sudoku[i] = this.shiftRow(sudoku[i - 1], 1);
+            } else sudoku[i] = this.shiftRow(sudoku[i - 1], 3);
+        }
+        
+        for (int i = 0; i < 9; i++) {
+            this.shuffle(sudoku);
         }
         
         return sudoku;
     }
     
-    private int[] generateRandomisedLine() {
+    public void shuffle(int[][] sudoku) {
+        Random rgen = new Random();
+        
+        // shuffling single rows
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                int row1 = (i * 3) + rgen.nextInt(2);
+                int row2 = (i * 3) + rgen.nextInt(2);
+                
+                this.swapRow(sudoku, row1, row2);
+            }
+        }
+        
+        // shuffling rowblocks
+        for (int i = 0; i < 9; i++) {
+            int rowblock1 = rgen.nextInt(2);
+            int rowblock2 = rgen.nextInt(2);
+            
+            this.swapRowBlock(sudoku, rowblock1, rowblock2);
+        }
+        
+        // shuffling single columns
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                int column1 = (i * 3) + rgen.nextInt(2);
+                int column2 = (i * 3) + rgen.nextInt(2);
+                
+                this.swapColumn(sudoku, column1, column2);
+            }
+        }
+        
+        // shuffling columnblocks
+        for (int i = 0; i < 9; i++) {
+            int columnblock1 = rgen.nextInt(2);
+            int columnblock2 = rgen.nextInt(2);
+            
+            this.swapColumnBlock(sudoku, columnblock1, columnblock2);
+        }
+    }
+    
+    private int[] generateRandomisedRow() {
         int[] randomisedLine = new int[9];
         
         for (int i = 0; i < randomisedLine.length; i++) {
@@ -40,7 +84,7 @@ public class SudokuGenerator {
         return randomisedLine;
     }
     
-    private int[] shiftLine(int[] line, int howMany) {
+    private int[] shiftRow(int[] line, int howMany) {
         int[] newLine = new int[line.length];
         
         for (int i = 0; i < line.length; i++) {
@@ -54,6 +98,36 @@ public class SudokuGenerator {
         }
         
         return newLine;
+    }
+    
+    private void swapRow(int[][] sudoku, int row1, int row2) {
+        int[] temp = sudoku[row2];
+        sudoku[row2] = sudoku[row1];
+        sudoku[row1] = temp;
+    }
+    
+    // rowblocks are groups of three rows
+    // rowblock with a value of 0 means rows 0-2, 1 means 3-5, and 2 means 6-8
+    private void swapRowBlock(int[][] sudoku, int rowblock1, int rowblock2) {
+        for (int i = 0; i < 3; i++) {
+            this.swapRow(sudoku, (rowblock1 * 3) + i, (rowblock2 * 3) + i);
+        }
+    }
+    
+    private void swapColumn(int[][] sudoku, int column1, int column2) {
+        for (int i = 0; i < sudoku[0].length; i++) {
+            int temp = sudoku[i][column2];
+            sudoku[i][column2] = sudoku[i][column1]; 
+            sudoku[i][column1] = temp; 
+        }
+    }
+    
+    // columnblocks are groups of three columns
+    // columnblock with a value of 0 means columns 0-2, 1 means 3-5, and 2 means 6-8
+    private void swapColumnBlock(int[][] sudoku, int columnblock1, int columnblock2) {
+        for (int i = 0; i < 3; i++) {
+            this.swapColumn(sudoku, (columnblock1 * 3) + i, (columnblock2 * 3) + i);
+        }
     }
     
     public void printSudoku(int[][] sudoku) {
