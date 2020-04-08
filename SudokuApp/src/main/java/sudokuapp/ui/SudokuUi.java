f4b52d43e023014330e26226a5a7ccc263457c01
Oskari Keränen <oskari.e.keranen@gmail.com>
2020-04-08 03:12:59 +0300

    improved gui

--- SudokuApp/src/main/java/sudokuapp/ui/SudokuUi.java
+++ SudokuApp/src/main/java/sudokuapp/ui/SudokuUi.java
@@ -2,6 +2,7 @@ package sudokuapp.ui;
 
 import java.util.HashMap;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
@@ -11,12 +12,18 @@ import sudokuapp.logic.SudokuGenerator;
 public class SudokuUi extends Application {
     private Scene sudokuScene;
     
-    private HashMap<Integer, Button> sudokuMap;
     private GridPane sudokuGrid;
+    
+    private HashMap<Integer, GridPane> gridMap;
+    private HashMap<Integer, Button> cellMap;
     
     @Override
     public void start(Stage stage) {
         sudokuGrid = new GridPane();
+        sudokuGrid.setHgap(5);
+        sudokuGrid.setVgap(5);
+        sudokuGrid.setPadding(new Insets(10, 10, 10, 10));
+        
         this.createGrid();
         
         sudokuScene = new Scene(sudokuGrid);
@@ -30,14 +37,24 @@ public class SudokuUi extends Application {
         SudokuGenerator generator = new SudokuGenerator();
         int[][] sudoku = generator.generateSudoku();
         
-        sudokuMap = new HashMap<>();
+        gridMap = new HashMap<>();
         
-        for (int x = 0; x < 9; x++) {
-            for (int y = 0; y < 9; y++) {
+        for (int i = 0; i < 9; i++) {
+            GridPane gridpane = new GridPane();
+            
+            gridMap.put(i, gridpane);
+            sudokuGrid.add(gridpane, i % 3, i / 3);
+        }
+        
+        cellMap = new HashMap<>();
+        
+        for (int y = 0; y < 9; y++) {
+            for (int x = 0; x < 9; x++) {
                 Button button = new Button("" + sudoku[y][x]);
+                int grid = ((y * 9 + x) / 3) % 3 + ((y * 9 + x) / 3) / 3 - ((y * 9 + x) / 9) % 3;
                 
-                sudokuMap.put(y * 9 + x, button);
-                sudokuGrid.add(button, x, y);
+                cellMap.put(y * 9 + x, button);
+                gridMap.get(grid).add(button, x, y);
             }
         }
     }
